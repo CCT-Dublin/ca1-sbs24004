2a14aaddda5faeab3db8283e4698afe7614b7143
sbs24004 <163578206+sbs24004@users.noreply.github.com>
2024-04-03 15:32:56 +0100

    Statistics

--- .ipynb_checkpoints/UserDefinedFunctions-checkpoint.py
+++ .ipynb_checkpoints/UserDefinedFunctions-checkpoint.py
@@ -4,3 +4,13 @@ def remove_IN_OUT(df):
     df_Totals = df.drop(columns=dropColumns)        # Remove columns, column axis, leaving only total footfall counts
                                                         # return dropColumns #- (test)
     return df_Totals
+
+
+def outlier_dates(df_column):
+    iq1 = df_column.quantile(0.25)    # Define lower quantile
+    iq3 = df_column.quantile(0.75)    # Define upper quantile
+    iqr = iq3 - iq1                     # Define interquartile range
+  
+    outliers = df_column[(df_column > (iq3 + (1.5 * iqr)))]  # return outliers greater than normal, to identify periods of high activity
+    
+    return outliers
